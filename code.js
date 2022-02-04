4211be5c412c40a1dc11f85bde27257b00feb754
NickWilder <nickwilder208@gmail.com>
2022-02-04 16:04:03 -0600

    roulette wip

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Message } = require('discord.js');
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
@@ -19,20 +19,18 @@ client.on('presenceUpdate', (oldMember, newMember) => {
 
   if(newMember.activities.length === 0) return; 
 
-  console.log(newMember);
-
   var game = newMember.activities[0].name;
   game = game.toLowerCase();
 
-  if(game.includes('bloons')) {
+  if(game.includes('league of legends')) {
     setInterval(() => {
       game = newMember.activities[0].name;
       game = game.toLowerCase();
-      if(game.includes('bloons')) {
+      if(game.includes('league of legends')) {
         var user = client.users.cache.get(newMember.userId);
         var guild = newMember.guild;
-        var channel = guild.channels.cache.find(ch => ch.name === 'bot-testing');
-        channel.send(`${user} has been playing Bloons for over an hour! Touch some grass! :RIPBOZO: :pepeLaughPoint: `)
+        var channel = guild.channels.cache.find(ch => ch.name === 'general');
+        channel.send(`${user} has been playing League of Legends for over an hour! Touch some grass! :joy: :joy: :joy:`)
       } else {
         return;
       }
@@ -41,6 +39,50 @@ client.on('presenceUpdate', (oldMember, newMember) => {
 
 });
 
+
+
+
+
+
+client.on('messageCreate', msg => {
+  var msgStr = msg.content.toLowerCase();
+  var user = client.users.cache.get(msg.author.id);
+
+  switch(msgStr) {
+    case '!ping':
+      msg.channel.send(`${user} Pong!`);
+      break;
+    case '!roulette':
+      var roll = Math.floor(Math.random() * 5) + 1;
+      setTimeout(() => {
+        msg.channel.send(`${user} you stand up against the wall, you take one final deep breath...`);
+      }, 4000);
+      setTimeout(() => {
+        msg.channel.send(`Nick chambers his round into his gun. Mutters under his breath, "I never thought I'd be so lucky."`);
+      }, 2000);
+      
+      //User wins a free kick
+      if(roll === 1) {
+        setTimeout(() => {
+          msg.channel.send(`BANG! The shot strikes you in the back. You fall to the ground, get out of this server`);
+        }, 3000);
+
+        msg.guild.fetchMember(msg.author.id).then(member => {
+          member.kick('Luck was not on your side today. https://discord.gg/jcX5hwFJbk').then(m => {
+            msg.channel.send(`${user} soul has been offered to the god's.`);
+          });
+        });
+      } else {
+      //User loses
+        setTimeout(() => {
+          msg.channel.send(`THUD! The shot strikes to the right of you. You live to see another day.`);
+        }, 3000);
+      }
+      break;
+  }
+  
+
+});
 
 
 client.login(process.env.TOKEN);
